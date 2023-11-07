fa7c80978aecb7f65decd07d19ebfdabd5d456fd
prajwalgowda900 <gprajwal822@gmail.com>
2023-11-08 00:28:50 +0530

    first commit

---
+++ Demo-1.java
@@ -0,0 +1,16 @@
+class Demo {
+   
+            public static void main(String[ ] args)  {
+                  int main=100;
+                       int a=200;
+                       int b=300;
+
+
+           System.out.println("Hii Prajwal Gowda");
+           System.out.println(main);
+           System.out.println(a);
+           System.out.println(b);
+
+
+       }
+}        
