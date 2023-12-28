a33ef85701866cd283aafc4f21467ebb3ea28c1b
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2023-12-28 23:31:04 +0530

    Update TestOne.java

--- interface/TestOne.java
+++ interface/TestOne.java
@@ -3,9 +3,9 @@ interface TestI{
     void m1();
 }
 class TestImpl implements TestI{
-   public void m1(){
+   public void m1()
     System.out.println("Impl Class - m1 method");
-   }
+   
    public static void main(String[] args){
             new TestImpl().m1();
    }   
