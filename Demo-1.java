dfbfb09b8092069056cf2c42e01371410c657d93
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2024-01-06 22:00:45 +0530

    Update Demo-1.java

--- Demo-1.java
+++ Demo-1.java
@@ -1,6 +1,6 @@
 class Demo {
    
-            public static void main(String[ ] args)  {
+            public static void main(String[ ] args)  
                   int main=100;
                        int a=200;
                        int b=300;
