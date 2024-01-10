d12423710fa20484f32499d5bea4ec89a72322a2
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2024-01-10 16:05:16 +0530

    Add files via upload

---
+++ Conditional.flow/Test.java
@@ -0,0 +1,12 @@
+class Test{
+    public static void main(String[] args){ 
+           String ename="Rajni";         
+           if(ename =="Rajni"){
+                System.out.println("GM");
+           }
+           else{
+            System.out.println("GN");
+           }
+
+    }
+}
