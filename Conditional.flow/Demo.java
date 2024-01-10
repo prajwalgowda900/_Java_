a860ff4b3409279a1f689bf557081cffad517f25
Prajwal Gowda <142891668+prajwalgowda900@users.noreply.github.com>
2024-01-10 16:00:45 +0530

    Add files via upload

---
+++ Conditional.flow/Demo.java
@@ -0,0 +1,13 @@
+class Demo{
+
+    public static void main(String[] args){
+        ///int num = args[0];
+        int num = Integer.parseInt(args[0]);
+        if(num % 2 ==0){
+                System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+    }
+}
